57e72645631c3a4c40d3d0189984c4b10734d4eb
fedef2000 <fedef306@gmail.com>
2022-08-24 15:18:12 +0200

    commit

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://stellular-tiramisu-05e0cc.netlify.app/'
+    'https://stellular-tiramisu-05e0cc.netlify.app/',
+    'https://main--stellular-tiramisu-05e0cc.netlify.app'
 ]
 
 module.exports = allowedOrigins
