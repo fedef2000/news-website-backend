e045eca32927d3860c90e3af4aa961a1d674f44e
fedef2000 <fedef306@gmail.com>
2022-08-29 09:53:26 +0200

    mobile dev origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
     'http://localhost:3000',
+    '192.168.1.13:3000',
     'https://sindacodelcalciomercato.netlify.app',
     'https://www.sindacodelcalciomercato.it',
     'https://sindacodelcalciomercato.it'
